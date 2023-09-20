b41de47d80400d60bf54f9f05ab9092813097275
Paras Roy <parasandroy@gmail.com>
2023-09-20 10:45:37 +0530

    small update

--- app/analysis/page.jsx
+++ app/analysis/page.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Page() {
   return (
-    <div>Analysis</div>
+    <Image src="https://cdn.dribbble.com/users/1998175/screenshots/9963575/media/afc56bd6a8a467ec05682788371ad897.png" alt="Picture of the author" width={1500} height={1500} />
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,46 @@
+import React from 'react';
+
 export default function Home() {
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    background: '#f0f0f0', // Background color
+  };
 
+  const headingStyle = {
+    fontSize: '6rem',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: '20px', // Adjust as needed
+    background: '-webkit-linear-gradient(45deg, #FF9A8B, #FF6A88, #FF476F)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    letterSpacing: '3px', // Adjust as needed
+    textShadow: '3px 3px 6px rgba(0, 0, 0, 0.2)', // Text shadow
+  };
 
+  const taglineStyle = {
+    fontSize: '1.8rem',
+    textAlign: 'center',
+    marginTop: '10px', // Adjust as needed
+  };
+
+  const teamNameStyle = {
+    fontSize: '2.5rem',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: '30px', // Adjust as needed
+  };
 
   return (
-        <>
-
-        <h1 className="text-4xl h-screen">News Sentinel</h1>
-        
-        </>
-  )
+    <div style={containerStyle}>
+      <span className='text-gray-500 text-sm'>Temporary design</span>
+      <h1 style={headingStyle}>News Sentinel</h1>
+      <p style={taglineStyle}>Media Feedback Powered by AI</p>
+      <p style={teamNameStyle}>TEAM GATIMAN</p>
+    </div>
+  );
 }
